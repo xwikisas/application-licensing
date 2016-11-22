43e1978681a576a12de0c52a464f5778f4af10d5
Vincent Massol <vincent@massol.net>
2016-11-22 16:52:07 +0100

    LICENSING-38: Add a UI for managing license details and generating licenses * Forgot to update the test

--- application-licensing-test/application-licensing-test-tests/src/test/it/com/xwiki/licensing/test/ui/LicensingTest.java
+++ application-licensing-test/application-licensing-test-tests/src/test/it/com/xwiki/licensing/test/ui/LicensingTest.java
@@ -110,7 +110,7 @@ public class LicensingTest extends AbstractTest
         content = "{{include reference='License.Code.LicenseDetailsMacros'/}}\n"
             + "\n"
             + "{{velocity}}\n"
-            + "#addLicenseDetails('John' 'john@acme.com' 'b6ad6165-daaf-41a1-8a3f-9aa81451c402' "
+            + "#addLicenseDetails('John' 'Doe' 'john@acme.com' 'b6ad6165-daaf-41a1-8a3f-9aa81451c402' "
                 + "'Active Directory Application', 'com.xwiki.activedirectory:application-activedirectory-main', '"
                     + "com.xwiki.activedirectory:application-activedirectory-api' 'trial' true)\n"
             + "success"
