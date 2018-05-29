ec89fecc07a7764bd8590763da9d5a1adb4a7535
Alex Cotiugă <alexandru.cotiuga@xwiki.com>
2018-05-29 13:47:10 +0300

    [Misc] Use logger to print the error message from ComponentLookupException

--- application-licensing-licensor/application-licensing-licensor-api/src/main/java/com/xwiki/licensing/internal/enforcer/DefaultLicensingSecurityCacheRuleInvalidator.java
+++ application-licensing-licensor/application-licensing-licensor-api/src/main/java/com/xwiki/licensing/internal/enforcer/DefaultLicensingSecurityCacheRuleInvalidator.java
@@ -181,6 +181,7 @@ public class DefaultLicensingSecurityCacheRuleInvalidator
                     componentManager.getInstance(ReadWriteLock.class, SECURITY_CACHE_RULES_INVALIDATOR_HINT);
             } catch (ComponentLookupException e) {
                 e.printStackTrace();
+                logger.error(e.getMessage(), e);
             }
         }
     }
