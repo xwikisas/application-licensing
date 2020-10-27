6a454869eec735a464454ef2736bed74179d041e
oanalavinia <florean.lavinia@gmail.com>
2020-10-27 17:58:37 +0200

    Automatic dependencies applications upgrades to new versions * added custom display for auto upgrades notifications * since NotificationsDisplayerClass is still unstable on older versions of xwiki the template is rendered from a java class for safety; this class should be removed after a xwiki parent upgrade

---
+++ application-licensing-licensor/application-licensing-licensor-api/src/main/java/com/xwiki/licensing/internal/upgrades/notifications/ExtensionAutoUpgradedEventDisplayer.java
@@ -0,0 +1,106 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package com.xwiki.licensing.internal.upgrades.notifications;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Provider;
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.LocalDocumentReference;
+import org.xwiki.notifications.CompositeEvent;
+import org.xwiki.notifications.NotificationException;
+import org.xwiki.notifications.notifiers.NotificationDisplayer;
+import org.xwiki.rendering.block.Block;
+import org.xwiki.template.Template;
+import org.xwiki.template.TemplateManager;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
+
+/**
+ * Displaying a custom template for ExtensionAutoUpgradedEvent. The NotificationDisplayerClass XObject should work
+ * independently of this class but it is unstable on older versions and sometimes the default template is rendered.
+ * After upgrading the 9.11 xwiki parent only the object should be used and this class removed.
+ * 
+ * @version $Id$
+ * @since 1.17
+ */
+@Component
+@Singleton
+@Named(ExtensionAutoUpgradedEventDisplayer.EVENT_TYPE)
+public class ExtensionAutoUpgradedEventDisplayer implements NotificationDisplayer
+{
+    /**
+     * The event type.
+     */
+    public static final String EVENT_TYPE =
+        "com.xwiki.licensing.internal.upgrades.notifications.ExtensionAutoUpgradedEvent";
+
+    private static final List<String> EVENTS = Arrays.asList(EVENT_TYPE);
+
+    private static final LocalDocumentReference DISPLAYER_DOC =
+        new LocalDocumentReference(Arrays.asList("Licenses", "Code"), "ExtensionAutoUpgradedEventDisplayer");
+
+    private static final LocalDocumentReference DISPLAYER_OBJ =
+        new LocalDocumentReference(Arrays.asList("XWiki", "Notifications", "Code"), "NotificationDisplayerClass");
+
+    @Inject
+    private TemplateManager templateManager;
+
+    @Inject
+    private Provider<XWikiContext> contextProvider;
+
+    @Inject
+    private Logger logger;
+
+    @Override
+    public Block renderNotification(CompositeEvent eventNotification) throws NotificationException
+    {
+        XWikiContext xcontext = contextProvider.get();
+
+        try {
+            XWikiDocument displayerDoc = xcontext.getWiki().getDocument(DISPLAYER_DOC, xcontext);
+            BaseObject displayerObj = displayerDoc.getXObject(DISPLAYER_OBJ, 0);
+
+            Template customTemplate = templateManager.createStringTemplate(
+                displayerObj.getStringValue("notificationTemplate"), displayerDoc.getDocumentReference());
+
+            return (customTemplate != null) ? this.templateManager.execute(customTemplate)
+                : this.templateManager.execute("notification/default.vm");
+        } catch (Exception e) {
+            logger.error("Failed to render the template for ExtensionAutoUpgradedEvent", e);
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> getSupportedEvents()
+    {
+        return EVENTS;
+    }
+
+}
