bec9c11e5ac8037d3bea8f05bd4b9d0c049b2985
Teo Caras <45433221+trrenty@users.noreply.github.com>
2025-02-13 11:22:30 +0200

    Missing methods in the licensor testing api #151 (#184)
    * [Misc] Removed unecessary version of pom.

--- application-licensing-test/application-licensing-test-api/src/main/java/com/xwiki/licensing/Licensor.java
+++ application-licensing-test/application-licensing-test-api/src/main/java/com/xwiki/licensing/Licensor.java
@@ -25,7 +25,7 @@ import org.xwiki.model.reference.EntityReference;
 
 /**
  * The licensor component used while running functional tests for licensed applications.
- * 
+ *
  * @version $Id$
  * @since 1.21
  */
@@ -47,4 +47,32 @@ public interface Licensor
      * @return true if the given extension has a valid license or is not subject to licensing.
      */
     boolean hasLicensure(ExtensionId extensionId);
+
+    /**
+     * @return true if the current document has a valid license or is not subject to licensing.
+     */
+    boolean hasLicensure();
+
+    /**
+     * Retrieve the currently applicable license for the current context document if any.
+     *
+     * @return a license, or null if there is no current document, or the current document is not subject to licensing.
+     */
+    License getLicense();
+
+    /**
+     * Retrieve the currently applicable license for the given installed extension.
+     *
+     * @param extensionId identifier of an installed extension
+     * @return a license, or null if the given installed extension is not subject to licensing.
+     */
+    License getLicense(ExtensionId extensionId);
+
+    /**
+     * Get the license applicable to the given reference.
+     *
+     * @param reference the reference to get the license from.
+     * @return a license, or null if the given reference is not subject to licensing.
+     */
+    License getLicense(EntityReference reference);
 }
--- application-licensing-test/application-licensing-test-api/src/main/java/com/xwiki/licensing/test/script/LicensorScriptService.java
+++ application-licensing-test/application-licensing-test-api/src/main/java/com/xwiki/licensing/test/script/LicensorScriptService.java
@@ -29,6 +29,7 @@ import org.xwiki.model.reference.EntityReference;
 import org.xwiki.script.service.ScriptService;
 import org.xwiki.stability.Unstable;
 
+import com.xwiki.licensing.License;
 import com.xwiki.licensing.Licensor;
 
 /**
@@ -47,26 +48,70 @@ public class LicensorScriptService implements ScriptService
     private Licensor licensor;
 
     /**
-     * Check if the given entity is covered by a valid license. Equivalent to licensor.hasLicensure(EntityReference)
-     * call.
+     * Retrieve the currently applicable license for the current context document if any. Equivalent to
+     * licensor.getLicense() call.
      *
-     * @param reference the reference of the entity for which licensure should be checked
-     * @return true if the given reference has a valid license or is not subject to licensing
+     * @return a license, or null if there is no current document, or the current document is not subject to licensing.
      */
-    public boolean hasLicensureForEntity(EntityReference reference)
+    public License getLicense()
     {
-        return this.licensor.hasLicensure(reference);
+        return licensor.getLicense();
+    }
+
+    /**
+     * Retrieve the currently applicable license for the given installed extension. Equivalent to
+     * licensor.getLicense(ExtensionId) call.
+     *
+     * @param extensionId identifier of an installed extension
+     * @return a license, or null if the given installed extension is not subject to licensing.
+     */
+    public License getLicenseForExtension(ExtensionId extensionId)
+    {
+        return licensor.getLicense(extensionId);
+    }
+
+    /**
+     * Get the license applicable to the given reference. Equivalent to licensor.getLicense(EntityReference) call.
+     *
+     * @param reference the reference to get the license from.
+     * @return a license, or null if the given reference is not subject to licensing.
+     */
+    public License getLicenseForEntity(EntityReference reference)
+    {
+        return licensor.getLicense(reference);
+    }
+
+    /**
+     * Equivalent to licensor.hasLicensure() call.
+     *
+     * @return true if the current document has a valid license or is not subject to licensing.
+     */
+    public boolean hasLicensure()
+    {
+        return licensor.hasLicensure();
     }
 
     /**
      * Check if the given extension is covered by a valid license. Equivalent to licensor.hasLicensure(ExtensionId)
      * call.
      *
-     * @param extensionId the identifier of the extension for which licensure should be checked
-     * @return true if the given extension has a valid license or is not subject to licensing
+     * @param extensionId the identifier of the extension for which licensure should be checked.
+     * @return true if the given extension has a valid license or is not subject to licensing.
      */
     public boolean hasLicensureForExtension(ExtensionId extensionId)
     {
-        return this.licensor.hasLicensure(extensionId);
+        return licensor.hasLicensure(extensionId);
+    }
+
+    /**
+     * Check if the given entity is covered by a valid license. Equivalent to licensor.hasLicensure(EntityReference)
+     * call.
+     *
+     * @param reference the reference of the entity for which licensure should be checked.
+     * @return true if the given reference has a valid license or is not subject to licensing.
+     */
+    public boolean hasLicensureForEntity(EntityReference reference)
+    {
+        return licensor.hasLicensure(reference);
     }
 }
